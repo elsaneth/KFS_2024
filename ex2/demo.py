205f4f9bccbf3696a9f59357ded29715e1bbd073
elsaneth <elisabeth.suits@gmail.com>
2024-03-01 11:27:54 +0200

    modification

--- ex2/demo.py
+++ ex2/demo.py
@@ -7,7 +7,7 @@ def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =",str(message.payload.decode("utf-8")))
 
-client= paho.Client("client-1") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
 
 # Bind function to callback
 client.on_message=on_message
